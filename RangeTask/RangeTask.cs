cfd9447029207d9a54e5f4ab9884e69edf63959e
Алексей Овчаренко <frog4231.96@mail.ru>
2025-07-15 23:50:30 +0700

    Range из основ программирования, без редактирования

--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -2,9 +2,31 @@
 {
     internal class RangeTask
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Range range = new(3.14, 14.7);
+
+            Console.WriteLine("Длина диапазона = " + range.GetLength());
+
+            Console.WriteLine("Измените диапазон, введите начало диапазона:");
+            range.From = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Измените диапазон, введите конец диапазона:");
+            range.To = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Длина диапазона = " + range.GetLength());
+
+            Console.WriteLine("Введите число для проверки нахождения числа в данном диапазоне:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Число внутри диапазона");
+            }
+            else
+            {
+                Console.WriteLine("Число не принадлежит диапазону");
+            }
         }
     }
 }
