1382c4e15ff73e5cc6830af4e1dd1831c650bb1e
Алексей Овчаренко <frog4231.96@mail.ru>
2025-07-17 00:06:28 +0700

    выполнение Range* и немного изменил проверку

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,4 +1,6 @@
-﻿namespace RangeTask
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace RangeTask
 {
     internal class Range
     {
@@ -20,6 +22,70 @@
         public bool IsInside(double number)
         {
             return number >= From && number <= To;
+        }
+
+        public Range GetIntersection(Range range)
+        {
+            if (range.To <= From || To <= range.From)
+            {
+                return null;
+            }
+
+            double maxFrom = Math.Max(From, range.From);
+            double minTo = Math.Min(To, range.To);
+
+            return new Range(maxFrom, minTo);
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            if (range.To < From || To < range.From)
+            {
+                Range range1 = new(From, To);
+                Range range2 = new(range.From, range.To);
+
+                return new Range[2] { range1, range2 };
+            }
+
+            double minFrom = Math.Min(From, range.From);
+            double maxTo = Math.Max(To, range.To);
+            Range rangesUnion = new(minFrom, maxTo);
+
+            return new Range[1] { rangesUnion };
+        }
+
+        public Range[] GetDiffirence(Range range)
+        {
+            if (range.To <= From || To <= range.From)
+            {
+                Range rangesDifference = new(From, To);
+
+                return new Range[1] { rangesDifference };
+            }
+
+            if ((range.From < To && range.From != From) && range.To >= To)
+            {
+                Range rangesDifference = new(From, range.From);
+
+                return new Range[1] { rangesDifference };
+            }
+
+            if (range.From <= From && (range.To > From && range.To != To))
+            {
+                Range rangesDifference = new(range.To, To);
+
+                return new Range[1] { rangesDifference };
+            }
+
+            if ((range.From > From && range.From < To) && (range.To > From && range.To < To))
+            {
+                Range rangesDifference1 = new(From, range.From);
+                Range rangesDifference2 = new(range.To, To);
+
+                return new Range[2] { rangesDifference1, rangesDifference2 };
+            }
+
+            return null;
         }
     }
 }
--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -4,28 +4,74 @@
     {
         public static void Main()
         {
-            Range range = new(3.14, 14.7);
+            Console.WriteLine("Введите начало и конец диапазона:");
 
-            Console.WriteLine("Длина диапазона = " + range.GetLength());
+            Range range1 = new(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
 
-            Console.WriteLine("Измените диапазон, введите начало диапазона:");
-            range.From = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Измените диапазон, введите конец диапазона:");
-            range.To = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Длина диапазона = " + range.GetLength());
+            Console.WriteLine("Длина диапазона = " + range1.GetLength());
 
             Console.WriteLine("Введите число для проверки нахождения числа в данном диапазоне:");
             double number = Convert.ToDouble(Console.ReadLine());
 
-            if (range.IsInside(number))
+            if (range1.IsInside(number))
             {
-                Console.WriteLine("Число внутри диапазона");
+                Console.WriteLine("Число внутри диапазона!");
             }
             else
             {
-                Console.WriteLine("Число не принадлежит диапазону");
+                Console.WriteLine("Число не принадлежит диапазону!");
+            }
+
+            Console.WriteLine("Введите начало и конец второго диапазона:");
+
+            Range range2 = new(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Range intersection = range1.GetIntersection(range2);
+
+            Console.WriteLine("Проверка пересечения:");
+
+            if (intersection is not null)
+            {
+                Console.WriteLine($"{intersection.From}, {intersection.To}");
+            }
+            else
+            {
+                Console.WriteLine("Пересечения нет");
+            }
+
+            Range[][] unionsArray = { range1.GetUnion(range2) };
+
+            Console.WriteLine("Проверка объединения:");
+
+            foreach (Range[] testRangesArray in unionsArray)
+            {
+                if (testRangesArray.Length == 2)
+                {
+                    Console.WriteLine($"{testRangesArray[0].From}, {testRangesArray[0].To} и {testRangesArray[1].From}, {testRangesArray[1].To}");
+                }
+                else
+                {
+                    Console.WriteLine($"{testRangesArray[0].From}, {testRangesArray[0].To}");
+                }
+            }
+
+            Range[][] differencesArray = { range1.GetDiffirence(range2) };
+
+            Console.WriteLine("Проверка разности:");
+
+            foreach (Range[] testRangesArray in differencesArray)
+            {
+                if (testRangesArray is null)
+                {
+                    Console.WriteLine("Диапазон отсутствует");
+                }
+                else if (testRangesArray.Length == 2)
+                {
+                    Console.WriteLine($"{testRangesArray[0].From}, {testRangesArray[0].To} и {testRangesArray[1].From}, {testRangesArray[1].To}");
+                }
+                else
+                {
+                    Console.WriteLine($"{testRangesArray[0].From}, {testRangesArray[0].To}");
+                }
             }
         }
     }
