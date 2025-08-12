fbf2e2108f0ab7e50eaeae06c02f03d5febacd3c
Алексей Овчаренко <frog4231.96@mail.ru>
2025-08-12 21:45:40 +0700

    Исправления ArrayListHome

---
+++ ArrayListHome/ArrayListHome.cs
@@ -0,0 +1,63 @@
+﻿namespace ArrayListHomeTask;
+
+public class ArrayListHome
+{
+    public static List<string> ReadFileLines(string filePath)
+    {
+        List<string> fileLines = new List<string>();
+
+        try
+        {
+            using StreamReader reader = new(filePath);
+            string? currentLine;
+
+            while ((currentLine = reader.ReadLine()) != null)
+            {
+                fileLines.Add(currentLine);
+            }
+        }
+
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"Файл не найден: {ex.Message}");
+        }
+
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine($"Нет доступа к файлу: {ex.Message}");
+        }
+
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Ошибка ввода-вывода: {ex.Message}");
+        }
+
+        return fileLines;
+    }
+
+    public static void RemoveEvenNumbers(List<int> list)
+    {
+        for (int i = list.Count - 1; i >= 0; i--)
+        {
+            if (list[i] % 2 == 0)
+            {
+                list.RemoveAt(i);
+            }
+        }
+    }
+
+    public static List<int> GetNumbersWithoutDuplicates(List<int> list)
+    {
+        List<int> numbersWithoutDuplicates = new List<int>();
+
+        foreach (int number in list)
+        {
+            if (!numbersWithoutDuplicates.Contains(number))
+            {
+                numbersWithoutDuplicates.Add(number);
+            }
+        }
+
+        return numbersWithoutDuplicates;
+    }
+}
--- ArrayListHome/ArrayListHomeTask.cs
+++ ArrayListHome/ArrayListHomeTask.cs
@@ -1,56 +1,27 @@
-﻿using System.Linq;
+﻿namespace ArrayListHomeTask;
 
-namespace ArrayListHomeTask;
-
-internal class ArrayListHomeTask
+public class ArrayListHomeTask
 {
     static void Main(string[] args)
     {
-        using StreamReader reader = new StreamReader("..\\..\\..\\input.txt");
-
-        List<string> fileLines = new List<string>();
-
-        string currentLine;
-
-        while ((currentLine = reader.ReadLine()) != null)
-        {
-            fileLines.Add(currentLine);
-        }
-
         Console.WriteLine("Проверка первого пункта задачи:");
-        Console.WriteLine(string.Join(", ", fileLines));
+        Console.WriteLine(string.Join(", ", ArrayListHome.ReadFileLines("..\\..\\..\\input.txt")));
         Console.WriteLine();
 
-        List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-
-        for (int i = 0; i < integers.Count; i++)
-        {
-            if (integers[i] % 2 == 0)
-            {
-                integers.RemoveAt(i);
-            }
-        }
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 16, 16};
 
         Console.WriteLine("Проверка второго пункта задачи:");
-        Console.WriteLine(string.Join(", ", integers));
+        Console.WriteLine("Список до удалений: " + string.Join(", ", numbers));
+
+        ArrayListHome.RemoveEvenNumbers(numbers);
+
+        Console.WriteLine("Список после удалений: " + string.Join(", ", numbers));
         Console.WriteLine();
 
-        List<int> duplicateIntegers = new List<int> { 10, 2, 3, 4, 5, 7, 7, 9, 101, 10, 101, 13, 4, 164, 15, 164 };
-
-        List<int> integersWithoutDuplicates = new List<int>();
-
-        for (int i = 0; i < duplicateIntegers.Count; i++)
-        {
-            if (!integersWithoutDuplicates.Contains(duplicateIntegers[i]))
-            {
-                integersWithoutDuplicates.Add(duplicateIntegers[i]);
-            }
-        }
+        List<int> duplicateNumbers = new List<int> { 10, 2, 3, 4, 5, 7, 7, 9, 101, 10, 101, 13, 4, 164, 15, 164 };
 
         Console.WriteLine("Проверка третьего пункта задачи:");
-        Console.WriteLine("Список с повторяющимися целыми числами: " + string.Join(", ", duplicateIntegers));
-        Console.WriteLine("Список с целыми числами без повторений: " + string.Join(", ", integersWithoutDuplicates));
-        Console.WriteLine();
-
+        Console.WriteLine("Список с повторяющимися целыми числами: " + string.Join(", ", duplicateNumbers));
+        Console.WriteLine("Список с целыми числами без повторений: " + string.Join(", ", ArrayListHome.GetNumbersWithoutDuplicates(duplicateNumbers)));
     }
 }
